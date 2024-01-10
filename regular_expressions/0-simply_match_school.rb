6bd683ba4737d13eeae392890dd2c631c7b40ef1
dean-daryl <deandaryl7@gmail.com>
2024-01-10 16:54:10 +0200

    Task 1

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,18 @@
 #!/usr/bin/env ruby
 # Regex match for school
-puts ARGV[0].scan(/\bSchool\b/).join
+
+# Check if an argument is provided
+if ARGV.length == 1
+  # Use scan with the regular expression to find occurrences of the word "School"
+  result = ARGV[0].scan(/\bSchool\b)
+
+  # Check if there are any matches
+  if result.empty?
+    puts "No match found."
+  else
+    # Print the matched word(s)
+    puts result.join
+  end
+else
+  puts "Please provide exactly one argument."
+end
